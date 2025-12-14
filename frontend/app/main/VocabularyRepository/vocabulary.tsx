c63a2a6d1e4fad12b811280a902539080447076d
Nguyễn Đặng Đại Nam <vip.nam.vip159@gmail.com>
2025-12-14 15:28:53 +0700

    Fix kho tu vung theo mock data

--- frontend/app/main/VocabularyRepository/vocabulary.tsx
+++ frontend/app/main/VocabularyRepository/vocabulary.tsx
@@ -1,45 +1,34 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { MY_VOCABULARY, MyVocabularyItem } from '../../../src/data/mockData';
+import { VOCABULARIES } from '../../../src/data/mockData';
+import { Vocabulary } from '../../../src/types';
 import { Ionicons } from '@expo/vector-icons';
 
 
-const VocabularyItem = ({ item }: { item: MyVocabularyItem }) => (
+const VocabularyItem = ({ item }: { item: Vocabulary }) => (
   <View style={styles.itemContainer}>
     <View style={styles.wordHeader}>
       <Text style={styles.word}>{item.word}</Text>
-      {item.is_learned ? (
-        <View style={[styles.statusBadge, styles.statusLearned]}>
-          <Ionicons name="checkmark-circle" size={14} color="#27ae60" />
-          <Text style={[styles.statusText, { color: '#27ae60' }]}> Đã thuộc</Text>
-        </View>
-      ) : (
-        <View style={[styles.statusBadge, styles.statusNotLearned]}>
-          <Ionicons name="time-outline" size={14} color="#f39c12" />
-          <Text style={[styles.statusText, { color: '#f39c12' }]}> Cần ôn</Text>
-        </View>
-      )}
     </View>
     <Text style={styles.meaning}>- {item.meaning}</Text>
     <Text style={styles.sentence}>VD: "{item.ex_sentence}"</Text>
   </View>
 );
 
-
 export default function VocabularyScreen() {
   return (
     <View style={styles.container}>
       <FlatList
-        data={MY_VOCABULARY}
+        data={VOCABULARIES}
         renderItem={({ item }) => <VocabularyItem item={item} />}
-        keyExtractor={(item) => item.user_vocab_id.toString()}
+        keyExtractor={(item) => item.vocabulary_id.toString()}
         contentContainerStyle={styles.listContainer}
         ListHeaderComponent={
           <View style={styles.headerContainer}>
             <Ionicons name="book-outline" size={28} color="#2980b9" />
             <Text style={styles.headerTitle}>Từ Vựng Đã Lưu</Text>
             <Text style={styles.headerSubtitle}>
-                Tổng hợp tất cả các từ bạn đã lưu từ các bài học.
+              Tổng hợp tất cả các từ bạn đã lưu từ các bài học.
             </Text>
           </View>
         }
@@ -50,7 +39,6 @@ export default function VocabularyScreen() {
     </View>
   );
 }
-
 
 
 const styles = StyleSheet.create({
@@ -90,9 +78,6 @@ const styles = StyleSheet.create({
     elevation: 3,
   },
   wordHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
     marginBottom: 6,
   },
   word: {
@@ -111,6 +96,7 @@ const styles = StyleSheet.create({
     color: '#7f8c8d',
     lineHeight: 20,
   },
+
   statusBadge: {
     flexDirection: 'row',
     alignItems: 'center',
